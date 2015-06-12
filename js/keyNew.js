e949e75ffc957adcb51e460951b4ddafbd5dd041
SelfieWithMe <102102039@mail3.nccu.tw>
2015-06-12 15:39:31 +0800

    ffdfdf

---
+++ js/keyNew.js
@@ -0,0 +1,17 @@
+ window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1070568829637766',
+      xfbml      : true,
+      version    : 'v2.3'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+     Parse.initialize("3Ct29PJCet8L8kfhLvbLTJ3DLKRC61RAp7ysXFU6", "3yHxjy4EBgJTMWYBuP1T7rG4qOHiiOuAnokZB5jb");
