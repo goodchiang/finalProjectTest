88dc6d5b31cf8f5bccdb2cba05b5e2045f9a2c86
SelfieWithMe <102102039@mail3.nccu.tw>
2015-06-12 15:43:08 +0800

    fffff

--- js/logIn.js
+++ js/logIn.js
@@ -1,23 +1,23 @@
-//step1:https://www.parse.com/docs/js/guide#users-facebook-users
-// Initialize Parse
-Parse.initialize("3Ct29PJCet8L8kfhLvbLTJ3DLKRC61RAp7ysXFU6", "3yHxjy4EBgJTMWYBuP1T7rG4qOHiiOuAnokZB5jb");
-window.fbAsyncInit = function() {
-	Parse.FacebookUtils.init({ // this line replaces FB.init({
-		appId      : '376624765869068', // Facebook App ID
-		status     : true,  // check Facebook Login status
-		cookie     : true,  // enable cookies to allow Parse to access the session
-		xfbml      : true,  // initialize Facebook social plugins on the page
-		version    : 'v2.3' // point to the latest Facebook Graph API version
-	});
-    // Run code after the Facebook SDK is loaded.
-};
-function(d, s, id){
-	var js, fjs = d.getElementsByTagName(s)[0];
-	if (d.getElementById(id)) {return;}
-	js = d.createElement(s); js.id = id;
-	js.src = "//connect.facebook.net/en_US/sdk.js";
-	fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+// //step1:https://www.parse.com/docs/js/guide#users-facebook-users
+// // Initialize Parse
+// Parse.initialize("3Ct29PJCet8L8kfhLvbLTJ3DLKRC61RAp7ysXFU6", "3yHxjy4EBgJTMWYBuP1T7rG4qOHiiOuAnokZB5jb");
+// window.fbAsyncInit = function() {
+// 	Parse.FacebookUtils.init({ // this line replaces FB.init({
+// 		appId      : '376624765869068', // Facebook App ID
+// 		status     : true,  // check Facebook Login status
+// 		cookie     : true,  // enable cookies to allow Parse to access the session
+// 		xfbml      : true,  // initialize Facebook social plugins on the page
+// 		version    : 'v2.3' // point to the latest Facebook Graph API version
+// 	});
+//     // Run code after the Facebook SDK is loaded.
+// };
+// function(d, s, id){
+// 	var js, fjs = d.getElementsByTagName(s)[0];
+// 	if (d.getElementById(id)) {return;}
+// 	js = d.createElement(s); js.id = id;
+// 	js.src = "//connect.facebook.net/en_US/sdk.js";
+// 	fjs.parentNode.insertBefore(js, fjs);
+// }(document, 'script', 'facebook-jssdk'));
 
 
 $(document).ready(function(){
@@ -56,33 +56,33 @@ $(document).on('click','#logoutBtn',function(e){
 	logout();
 });
 
-function fblogin(){
-//step2:https://developers.facebook.com/docs/graph-api/using-graph-api/v2.3 
-	Parse.FacebookUtils.logIn(null, {
-		success: function(user) {
-			if (!user.existed()) {
-				alert("註冊完成並且透過臉書登入");
-			}else {
-				alert("透過臉書登入成功");
-			}
-			indexView();//切換至預先寫好登入後的介面
-			FB.getLoginStatus(function(response) {
-				if (response.status === 'connected') {
-					FB.api('/me/picture?type=large', function (response) {
-					    $('#fbPic').html(<img src="+response.data.url+" crossorigin=\"anonymous\" id=preview1 />");          
-			        });
-			        FB.api('/me', function (response) {
-						console.log(response);
-						$('#fbPic').append("<h1>Welcome , "+(response['gender']=="male"?"Mr. ":"Miss ")+" "+response['first_name']+"</h1>");
-			        });	           
-				}
-			}); 
-		},
-		error: function(user, error) {
-			alert("使用者取消登入或沒有授權");
-		}
-	});
-}
+// function fblogin(){
+// //step2:https://developers.facebook.com/docs/graph-api/using-graph-api/v2.3 
+// 	Parse.FacebookUtils.logIn(null, {
+// 		success: function(user) {
+// 			if (!user.existed()) {
+// 				alert("註冊完成並且透過臉書登入");
+// 			}else {
+// 				alert("透過臉書登入成功");
+// 			}
+// 			indexView();//切換至預先寫好登入後的介面
+// 			FB.getLoginStatus(function(response) {
+// 				if (response.status === 'connected') {
+// 					FB.api('/me/picture?type=large', function (response) {
+// 					    $('#fbPic').html(<img src="+response.data.url+" crossorigin=\"anonymous\" id=preview1 />");          
+// 			        });
+// 			        FB.api('/me', function (response) {
+// 						console.log(response);
+// 						$('#fbPic').append("<h1>Welcome , "+(response['gender']=="male"?"Mr. ":"Miss ")+" "+response['first_name']+"</h1>");
+// 			        });	           
+// 				}
+// 			}); 
+// 		},
+// 		error: function(user, error) {
+// 			alert("使用者取消登入或沒有授權");
+// 		}
+// 	});
+// }
 
 function loginView(){
 	$('#logoutBtn').hide();
@@ -90,7 +90,7 @@ function loginView(){
 	
 	$('#loginView').show();
 	$('.notLogin').show();
-	//$('body').css("background-image","url(img/background.jpg)");
+	$('body').css("background-image","url(img/background.jpg)");
 	$('#fbPic').html('');
 	
 }
@@ -101,7 +101,7 @@ function indexView(){
 	
 	$('#indexView').show();
 	$('#logoutBtn').show();
-	//$('body').css("background-image","url(img/index.jpg)");
+	$('body').css("background-image","url(img/index.jpg)");
 }
 
 function logout(){
